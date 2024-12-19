ce5df5c1a0ec57e1d2af99f4a0c9d7319b2982e0
igor Gladkikh <kla2013@rambler.ru>
2024-12-19 18:48:52 +0300

    Вынести логику работы с загрузочным файлом в утилитный класс

---
+++ src/main/java/com/igladkikh/warehouse/util/FileUtil.java
@@ -0,0 +1,34 @@
+package com.igladkikh.warehouse.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.lang.System.getProperty;
+
+public class FileUtil {
+    private static final String MIME_TYPE = "text/csv";
+    private static final Path TMP_DIRECTORY = Paths.get(getProperty("user.dir"), "tmp");;
+
+    public static void validate(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new RuntimeException("Uploaded file is empty");
+        }
+
+        String mimeType = file.getContentType();
+        if (!MIME_TYPE.equals(mimeType)) {
+            throw new RuntimeException("Invalid uploaded file mime type");
+        }
+    }
+
+    public static void createTempFile(MultipartFile file) throws IOException {
+        if (Files.notExists(TMP_DIRECTORY)) {
+            Files.createDirectories(TMP_DIRECTORY);
+        }
+        Path tmpFile = Files.createTempFile(TMP_DIRECTORY, "upload", ".tmp");
+        file.transferTo(tmpFile);
+    }
+}
