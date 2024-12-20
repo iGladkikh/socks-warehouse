f131c231d1b1851bb5f2e0c21298f03f4d7ff2b7
Igor Gladkikh <kla2005@yandex.ru>
2024-12-20 16:07:36 +0300

    Обновить слой репозитория

--- src/main/java/com/igladkikh/warehouse/repository/SockRepository.java
+++ src/main/java/com/igladkikh/warehouse/repository/SockRepository.java
@@ -2,11 +2,34 @@ package com.igladkikh.warehouse.repository;
 
 import com.igladkikh.warehouse.model.Sock;
 import com.igladkikh.warehouse.model.SockColor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface SockRepository extends JpaRepository<Sock, Long> {
+public interface SockRepository extends JpaRepository<Sock, Long>, JpaSpecificationExecutor<Sock> {
 
     Optional<Sock> findByColorAndCottonPercentPart(SockColor color, int cotton);
+
+    default Specification<Sock> hasColor(List<SockColor> colors) {
+        return (sock, query, builder) ->
+                colors == null ? null : sock.get("color").in(colors);
+    }
+
+    default Specification<Sock> cottonEquals(Integer cotton) {
+        return (sock, query, builder) ->
+                cotton == null ? null : builder.equal(sock.get("cottonPercentPart"), cotton);
+    }
+
+    default Specification<Sock> cottonLessThan(Integer cotton) {
+        return (sock, query, builder) ->
+                cotton == null ? null : builder.lessThan(sock.get("cottonPercentPart"), cotton);
+    }
+
+    default Specification<Sock> cottonGreaterThan(Integer cotton) {
+        return (sock, query, builder) ->
+                cotton == null ? null : builder.greaterThan(sock.get("cottonPercentPart"), cotton);
+    }
 }
